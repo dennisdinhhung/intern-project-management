008cdcfb5ffd33eaead8b8a9e80c9c05164d6f29
dennisdinhhung <dennisdinhhung@mgmail.com>
2022-05-20 08:30:22 +0700

    add: home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,46 @@ function Home() {
   return (
     <div className='Home Common'>
       <div className="title">
-        Home
+        Welcome to NotJira
       </div>
+
+      <div className="div-home">
+
+        <div className='section'>
+          <div className="title-2">
+            Introduction
+          </div>
+
+
+          <div className='text-section introduction'>
+            <img src="NotJira Menu Img.png" alt="Menu Introduction" className='intro-pic' />
+
+            <div>
+              <div className='new'>
+                Are you new to NotJira?
+              </div>
+
+              <div className='text'>
+                Not sure where to start? Check out the <span>101 guide</span> here
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='section'>
+          <div className="title-2">
+            Announcement
+          </div>
+
+          <div className='text-section'>
+            <div>
+              There is no announcement
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   )
 }
